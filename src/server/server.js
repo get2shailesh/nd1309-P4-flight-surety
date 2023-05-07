625b962e6e3b692545a6021e14d29b8cff930a14
Shailesh Deshpande <get2shailesh@gmail.com>
2023-05-07 16:26:12 -0400

    Add files via upload
    Modified the code to add randomization to status code for oracle.

--- src/server/server.js
+++ src/server/server.js
@@ -9,6 +9,9 @@ let config = Config['localhost'];
 let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
 web3.eth.defaultAccount = web3.eth.accounts[0];
 let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+//SD: Adding statuses for randomization, with 20 status first..
+//SD: so it can be selected in a loop differently from the rest.
+const statuses = [20, 0, 10, 30, 40, 50];
 
 //SD: function to register Oracles
 async function registerOracles() {
@@ -25,6 +28,16 @@ async function registerOracles() {
 	console.log('[', accounts.length, '] Oracles registered');
 }
 
+//SD: Create a random status generator function
+const gerRandomFlightStatusCode = () =>{
+    let index = Math.floor(Math.random() * Math.floor(10));
+    if (index <= 7)
+        return statuses[0];
+    else {
+        return statuses[Math.floor(Math.random() * Math.floor(5)) + 1];
+    }    
+};
+
 //SD: function to mimic Oracle response as if it is coming from outside.
 async function simulateOracleResponse(requestedIndex, airline, flight, timestamp) {
 	const accounts = await web3.eth.getAccounts();
@@ -34,9 +47,10 @@ async function simulateOracleResponse(requestedIndex, airline, flight, timestamp
 		for (const index of indexes) {
 			try {
 				if (requestedIndex == index) {
+					let statusCode = gerRandomFlightStatusCode();
 					console.log("Submitting Oracle response For Flight: " + flight + " at Index: " + index);
 					await flightSuretyApp.methods.submitOracleResponse(
-						index, airline, flight, timestamp, 20
+						index, airline, flight, timestamp, statusCode
 					).send({ from: account, gas: 6721900 });
 
 				}
